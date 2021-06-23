6d6aaecfb813dbda37b79ad533e47cf5791cdacf
Alphonce-D <Teiio@hotmail.fr>
2021-06-23 16:21:40 +0200

    oui

---
+++ gridsome.server.js
@@ -0,0 +1,36 @@
+const nodeExternals = require("webpack-node-externals");
+const axios = require("axios");
+
+module.exports = function(api) {
+  api.chainWebpack((config, { isServer }) => {
+    if (isServer) {
+      config.externals([
+        nodeExternals({
+          allowlist: [/^vuetify/],
+        }),
+      ]);
+    }
+  });
+
+  
+  api.loadSource(async (actions) => {
+    const { data } = await axios.get("http://localhost:1337/products");
+    const collection = actions.addCollection({
+      typeName: "Product",
+    });
+    for (const product of data) {
+      collection.addNode({
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        rating: product.rating,
+        description: product.description,
+        image: product.image.formats.thumbnail.url,
+        instructions: product.instructions,
+      });
+    }
+  });
+  api.createPages(({ createPage }) => {
+    // Use the Pages API here: https://gridsome.org/docs/pages-api/
+  });
+}
